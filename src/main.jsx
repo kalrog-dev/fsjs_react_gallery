2ec0fbf64f82c34a354696698cb4e8861158e797
kalrog-dev <16406921+kalrog-dev@users.noreply.github.com>
2024-02-04 15:51:52 +0100

    Reset router basename

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/fsjs_react_gallery'>
+    <BrowserRouter>
       <App data={appData} />
     </BrowserRouter>
   </React.StrictMode>
